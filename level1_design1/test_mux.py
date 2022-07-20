9b1988ffaa83378bc0aa74009289b31fbcb06033
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-20 21:06:46 +0000

    updated test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,4 +7,16 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    Inp30 = 1
+    SEL = 30
+    
+    # input driving 
+    dut.sel.value = SEL
+    dut.inp30.value = Inp30
+    #dut.inp13.value = Inp13
+     
+    await Timer(2, units='ns')
+        
+    # cocotb.log.info('##### CTB: Develop your test here ########')
+    
+    assert dut.out.value == Inp30, f"Muliplexer has selected wrong input: {dut.sel.value} = 30"
