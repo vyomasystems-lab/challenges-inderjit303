fc9039a4bac460416ffec6dc9b8c0f69c77fd726
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-23 09:19:35 +0000

    updated file status

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -22,12 +22,15 @@ async def test0_basic_mux(dut):
     
     assert dut.out.value == Inp0, f"Muliplexer has selected wrong input: {dut.sel.value} != 0"
 
+    cocotb.log.info('##### CTB: Test 0 #####')
+
+
 @cocotb.test()
 async def test1_basic_mux(dut):
     """Test 1 for mux"""
 
     Inp1 = 1
-    SEL = 0
+    SEL = 1
     
     # input driving 
     dut.sel.value = SEL
@@ -40,20 +43,23 @@ async def test1_basic_mux(dut):
     
     assert dut.out.value == Inp1, f"Muliplexer has selected wrong input: {dut.sel.value} != 1"
 
-@cocotb.test()
-async def test30_basic_mux(dut):
-    """Test 30 for mux"""
+    cocotb.log.info('##### CTB: Test 1 #####')
 
-    Inp30 = 1
-    SEL = 30
+
+@cocotb.test()
+async def test2_basic_mux(dut):
+    """Test 2 for mux"""
+
+    Inp2 = 1
+    SEL = 2
     
     # input driving 
     dut.sel.value = SEL
-    dut.inp30.value = Inp30
+    dut.inp2.value = Inp2
     #dut.inp13.value = Inp13
      
     await Timer(6, units='ns')
         
-    # cocotb.log.info('##### CTB: Develop your test here ########')
-    
-    assert dut.out.value == Inp30, f"Muliplexer has selected wrong input: {dut.sel.value} != 30"
+    assert dut.out.value == Inp2, f"Muliplexer has selected wrong input: {dut.sel.value} != 2"
+
+    cocotb.log.info('##### CTB: Test 2 #####')
