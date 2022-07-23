12d80f525c97f5d025db851bc6b660150dc56a54
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-23 08:22:39 +0000

    updated file status

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,19 +5,19 @@ from cocotb.triggers import Timer
 import random
 
 @cocotb.test()
-async def test_basic_mux(dut):
-    """Test for mux"""
+async def test0_basic_mux(dut):
+    """Test 0 for mux"""
 
-    Inp30 = 1
-    SEL = 30
+    Inp0 = 1
+    SEL = 0
     
     # input driving 
     dut.sel.value = SEL
-    dut.inp30.value = Inp30
+    dut.inp0.value = Inp0
     #dut.inp13.value = Inp13
      
     await Timer(2, units='ns')
         
     # cocotb.log.info('##### CTB: Develop your test here ########')
     
-    assert dut.out.value == Inp30, f"Muliplexer has selected wrong input: {dut.sel.value} != 30"
+    assert dut.out.value == Inp0, f"Muliplexer has selected wrong input: {dut.sel.value} != 0"
