014cadee58b3edf59a59f615d5c61780ad70782e
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-23 17:33:09 +0000

    updated file status

--- level3_design/test_CLA32.py
+++ level3_design/test_CLA32.py
@@ -10,24 +10,28 @@ async def test_basic_CLA32(dut):
     """Test for 32 bit CLA adder which calculates a ='bd999; b='d98999"""
 
     # input driving 
-    a = 0x000003E7
-    b = 0x000182b7
-    cin = 0
-    dut.a.value = a
-    dut.b.value = b
-    dut.cin.value = cin
+    A = 999
+    B = 98999
+    Cin = 0
+    dut.a.value = A
+    dut.b.value = B
+    dut.cin.value = Cin
     
-    cocotb.log.info('a|b||cout|sum')
+    cocotb.log.info('A|B||cout|sum')
 
 
     await Timer(2, units='ns')
+     # run model and get the results
+    sum, cout = test_basic_CLA32(A, B, Cin)
 
-    assert dut.sum.value == a+b, f"Sum result is incorrect: {dut.sum.value} != 99998"
-    dut._log.info("Test passed!")
-
-    
-    #assert dut.out.value == Inp0, f"Muliplexer has selected wrong input: {dut.sel.value} != 0"
-
+    if dut.sum.value != sum:
+         x = "incorrect result for sum: {} != {}".format(dut.sum.value, int(sum))
+         raise TestFailure(x)
+    elif dut.cout.value != cout:
+        x = "incorrect result for carry: {} != {}".format(dut.cout.value, int(cout))
+        raise TestFailure(x)
+    else:
+        dut._log.info("Test is passed!")
     
 
 
