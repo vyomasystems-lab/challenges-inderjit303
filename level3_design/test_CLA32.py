a820fedf25abbbab8fe427d39e74953b030b1406
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-26 09:54:15 +0000

    updated file status

--- level3_design/test_CLA32.py
+++ level3_design/test_CLA32.py
@@ -15,7 +15,7 @@ async def test_CLA32_Basic(dut):
     dut.a.value = A
     dut.b.value = B
 
-    
+
     cocotb.log.info('A|B||cout|sum')
 
     await Timer(2, units='ns')
@@ -33,14 +33,14 @@ async def test_CLA32_randomise(dut):
         B = random.randint(0, 4294967295)
         
         A = 4294967295
-        B = 4294967295
+        B = 255
         dut.a.value = A
         dut.b.value = B
 
         await Timer(2, units='ns')
         
         dut._log.info(f'A={A:10} B={B:10} model={A+B:10} DUT={int(dut.sum.value):10}')
-        assert dut.sum.value == A+B, "Randomised test failed with: {A} + {B} = {SUM}".format(
+        assert dut.sum.value == A + B  , "Randomised test failed with: {A} + {B} = {SUM}".format(
             A=dut.a.value, B=dut.b.value, SUM=dut.sum.value)
         
     
