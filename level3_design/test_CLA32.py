fc7494e5f760ccd74896e8363259e7e3c17833cc
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-26 17:22:51 +0000

    updated file status

--- level3_design/test_CLA32.py
+++ level3_design/test_CLA32.py
@@ -3,7 +3,6 @@ import cocotb
 from cocotb.triggers import Timer
 import random
 
-
 @cocotb.test()
 async def test_CLA32_Basic(dut):
     """Test for 32 bit CLA adder which calculates a ='bd999; b='d98999"""
@@ -15,33 +14,32 @@ async def test_CLA32_Basic(dut):
     dut.a.value = A
     dut.b.value = B
 
-
     cocotb.log.info('A|B||cout|sum')
 
     await Timer(2, units='ns')
     
     assert dut.sum.value == A + B, "Adder result is incorrect: {A} + {B} != {SUM}".format(
-            A=int(dut.a.value), B=int(dut.b.value), SUM=int(dut.sum.value))
+            A=int(dut.a.value) , B=int(dut.b.value), SUM=int(dut.sum.value))
 
 
 @cocotb.test()
 async def test_CLA32_randomise(dut):
     """Test for adding two random numbers multiple times"""
 
-    for i in range(10):
+    for i in range(5):
 
         A = random.randint(0, 4294967295)
         B = random.randint(0, 4294967295)
         
-        A = 65535
-        B = 65535
+        A = 1100100011001010
+        B = 1100100011001010
         dut.a.value = A
         dut.b.value = B
 
         await Timer(2, units='ns')
         
-        dut._log.info(f'A={A:10} B={B:10} model={A+B:10} DUT={int(dut.sum.value):10}')
-        assert dut.sum.value == A + B  , "Randomised test failed with: {A} + {B} = {SUM}".format(
+        dut._log.info(f'A={A:05} B={B:05} model={A+B:05} DUT={int(dut.sum.value):05}')
+        assert dut.sum.value == A + B  , "Randomised test failed with: {A} + {B} != {SUM}".format(
             A=dut.a.value, B=dut.b.value, SUM=dut.sum.value)
         
     
