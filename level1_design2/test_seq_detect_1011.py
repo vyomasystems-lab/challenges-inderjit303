01a088243b3d34840a271ce3c3278208ad0ca7ef
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-29 20:29:06 +0000

    updated file status

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -12,7 +12,7 @@ from cocotb.triggers import RisingEdge, FallingEdge
 
 @cocotb.test()
 async def test_seq_bug1(dut):
-    """Test for seq detection """
+    """Test for 1011 sequence detection """
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
@@ -23,4 +23,23 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+    cocotb.log.info('#### CTB: Sequence detectir Testbench! ######')
+
+    # intitalize input
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"input_bit={dut.inp_bit.value}")
+    dut._log.info(f"current_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.current_state.value == dut.SEQ_1.value,f"Output is incorrect {dut.current_state}!={dut.SEQ_1.value}"
+    
+    
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"input_bit={dut.inp_bit.value}")
+    dut._log.info(f"current_state={dut.current_state.value}")
+    dut._log.info(f"next_state={dut.next_state.value}")
+    assert dut.current_state.value == dut.SEQ_1.value,f"Output is incorrect {dut.current_state}!={dut.SEQ_1.value}"
+
+    
--- test_8bit_CLA/test_adder.py
+++ test_8bit_CLA/test_adder.py
@@ -7,8 +7,8 @@ import random
 async def adder_basic_test(dut):
     """Test for 3 + 12"""
 
-    A = 180
-    B = 20
+    A = 3
+    B = 12
 
     # input driving
     dut.a.value = A
@@ -16,5 +16,4 @@ async def adder_basic_test(dut):
    
     await Timer(2, units='ns')
 
-    assert dut.sum.value == A+B, f"Adder result is incorrect: {dut.sum.value} != 255"
-
+    assert (dut.sum.value) == A+B, f"Adder result is incorrect: {dut.sum.value} != 15"
