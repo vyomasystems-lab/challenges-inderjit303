8125d7c4b27022885305e0289fd784a56c082e88
INDERJIT SINGH DHANJAL <99788755+inderjit303@users.noreply.github.com>
2022-07-30 21:09:38 +0530

    Update test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -12,7 +12,7 @@ from cocotb.triggers import RisingEdge, FallingEdge
 
 @cocotb.test()
 async def test_seq_bug1(dut):
-    """Test for 1011 sequence detection """
+    """Test 1 for 1011 sequence detection """
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
@@ -36,16 +36,23 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+    dut._log.info(f'Output sequence: {dut.seq_seen.value}')
 
-    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 11011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
+    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 110111. Model Output: {dut.seq_seen.value} Expected Output: 1'
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+    dut._log.info(f'Output sequence: {dut.seq_seen.value}')
+
 
 @cocotb.test()
 async def test_seq_bug2(dut):
-    """Test for seq detection """
+    """Test 2 for 1011 sequence detection"""
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
-    dut._log.info(f'Input bit: {dut.inp_bit.value}')
     
     # reset
     dut.reset.value = 1
@@ -67,7 +74,13 @@ async def test_seq_bug2(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
 
-    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 101011. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+    dut._log.info(f'Output sequence: {dut.seq_seen.value}')
+    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 1010111. Model Output: {dut.seq_seen.value} Expected Ouput: 1'
+    
+    
 
     
