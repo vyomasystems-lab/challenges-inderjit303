067b742d5c2c8f299d9426637341edddf5e5e35a
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-31 04:12:36 +0000

    updated file status

--- fixed_level1_design2/test_seq_detect_1011.py
+++ fixed_level1_design2/test_seq_detect_1011.py
@@ -28,31 +28,22 @@ async def test_seq_bug1(dut):
     # intitalize input
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
-    dut.inp_bit.value = 0
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 0
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 0
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 0
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await FallingEdge(dut.clk)
-
-    
     dut._log.info(f'Input bit: {dut.inp_bit.value}')
-    dut._log.info(f'Output sequence if 1 then SEQ = 1011: {dut.seq_seen.value}')
     dut._log.info(f'Current state: {dut.current_state.value}')
     dut._log.info(f'Next state: {dut.next_state.value}')
-    assert dut.seq_seen.value == 1, f'Sequence must be detected but is not detected. Given sequence = 110111. Model Output: {dut.seq_seen.value} Expected Output: 1'
+    assert dut.current_state.value == dut.SEQ_1.value, f"Sequence must be detected but is not detected {dut.current_state.value}!= {dut.SEQ_1.value}"
 
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+    dut._log.info(f'Current state: {dut.current_state.value}')
+    dut._log.info(f'Next state: {dut.next_state.value}')
+    assert dut.current_state.value == dut.SEQ_1.value, f"Sequence must be detected but is not detected {dut.current_state.value}!= {dut.SEQ_1.value}"
+
+    
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'Input bit: {dut.inp_bit.value}')
+    dut._log.info(f'Current state: {dut.current_state.value}')
+    dut._log.info(f'Next state: {dut.next_state.value}')
+    assert dut.current_state.value == dut.SEQ_1.value, f"Sequence must be detected but is not detected {dut.current_state.value}!= {dut.SEQ_1.value}"
