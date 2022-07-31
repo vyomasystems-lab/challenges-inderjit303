303054df9f40a64bdbf64278c50c9110a59c3d16
INDERJIT SINGH DHANJAL <inderjitsingh87@gmail.com>
2022-07-31 10:13:25 +0000

    updated file status

--- fixed_level1_design1/mux.v
+++ fixed_level1_design1/mux.v
@@ -56,7 +56,7 @@ module mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8,
       5'b11011: out = inp27;
       5'b11100: out = inp28;
       5'b11101: out = inp29;
-      5'b11110: out = inp29;
+      5'b11110: out = inp30;
     endcase
   end
 
